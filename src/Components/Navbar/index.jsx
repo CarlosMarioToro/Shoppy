5a717f34717e99bd86f599a1b851057a76b4c703
CarlosMarioToro <111100510+CarlosMarioToro@users.noreply.github.com>
2023-07-24 19:46:55 -0500

    Clase 7

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,36 +1,33 @@
-import { ShoppingBagIcon } from '@heroicons/react/24/solid'
 import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import { ShoppingCartContext } from '../../Context'
+import ShoppingCart from '../ShoppingCart'
 
 const Navbar = () => {
     const context = useContext(ShoppingCartContext);
     const activeStyle = 'underline underline-offset-4'
 
     // Sign Out
-  const signOut = localStorage.getItem('sign-out')
-  const parsedSignOut = JSON.parse(signOut)
-  const isUserSignOut = context.signOut || parsedSignOut
-  
-  const handleSignOut = () => {
+    const signOut = localStorage.getItem('sign-out')
+    const parsedSignOut = JSON.parse(signOut)
+    const isUserSignOut = context.signOut || parsedSignOut
+
+    // Account
+    const account = localStorage.getItem('account')
+    const parsedAccount = JSON.parse(account)
+    // Has an account
+    const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+    const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+    const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
+
+    const handleSignOut = () => {
         const stringifiedSignOut = JSON.stringify(true)
         localStorage.setItem('sign-out', stringifiedSignOut)
         context.setSignOut(true)
     }
 
     const renderView = () => {
-        if (isUserSignOut) {
-            return (
-                <li>
-                    <NavLink
-                        to="/sign-in"
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                        onClick={() => handleSignOut()}>
-                        Sign out
-                    </NavLink>
-                </li>
-            )
-        } else {
+        if (hasUserAnAccount && !isUserSignOut) {
             return (
                 <>
                     <li className='text-black/60'>
@@ -60,6 +57,17 @@ const Navbar = () => {
                     </li>
                 </>
             )
+        } else {
+            return (
+                <li>
+                    <NavLink
+                        to="/sign-in"
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
+                        onClick={() => handleSignOut()}>
+                        Sign in
+                    </NavLink>
+                </li>
+            )
         }
     }
 
@@ -67,7 +75,7 @@ const Navbar = () => {
         <nav className='flex justify-between items-center top-0 fixed z-10 w-full py-5 px-8 text-sm font-light bg-white'>
             <ul className='flex items-center gap-3'>
                 <li className='font-semibold text-lg'>
-                    <NavLink to='/'>
+                    <NavLink to={`${isUserSignOut ? '/sign-in' : '/'}`}>
                         Shopi
                     </NavLink>
                 </li>
@@ -135,10 +143,7 @@ const Navbar = () => {
             <ul className='flex items-center gap-3'>
                 {renderView()}
                 <li className='flex items-center'>
-                    <ShoppingBagIcon className='h-6 w-6 text-black/80'/>
-                    <div>
-                        {context.count}
-                    </div>
+                    <ShoppingCart />
                 </li>
             </ul>
         </nav>
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
-import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { useContext } from 'react'
+import { useRoutes, BrowserRouter, Navigate } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import SignIn from '../SignIn'
@@ -7,17 +8,30 @@ import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import Navbar from '../../Components/Navbar'
 import CheckoutSideMenu from '../../Components/CheckoutSideMenu'
-import { ShoppingCartProvider } from '../../Context'
+import { ShoppingCartProvider, initializeLocalStorage, ShoppingCartContext } from '../../Context'
 import './App.css'
 
 const AppRoutes = () => {
+  const context = useContext(ShoppingCartContext)
+  // Account
+  const account = localStorage.getItem('account')
+  const parsedAccount = JSON.parse(account)
+  // Sign Out
+  const signOut = localStorage.getItem('sign-out')
+  const parsedSignOut = JSON.parse(signOut)
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = Object.keys(context.account).length === 0
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
+  const isUserSignOut = context.signOut || parsedSignOut
+
   let routes = useRoutes([
-    { path: '/', element: <Home /> },
-    { path: '/clothes', element: <Home /> },
-    { path: '/electronics', element: <Home /> },
-    { path: '/furnitures', element: <Home /> },
-    { path: '/toys', element: <Home /> },
-    { path: '/others', element: <Home /> },
+    { path: '/', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/clothes', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/electronics', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/furnitures', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/toys', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/others', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
     { path: '/my-account', element: <MyAccount /> },
     { path: '/sign-in', element: <SignIn /> },
     { path: '/my-orders/:id', element: <MyOrder /> },
@@ -31,6 +45,8 @@ const AppRoutes = () => {
 }
 
 const App = () => {
+  initializeLocalStorage()
+  
   return (
     <ShoppingCartProvider>
       <BrowserRouter>
